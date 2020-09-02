c6fd191aec42bb1fd5ef03dbf8de749a0998d88a
DivyaDiya12 <70312772+DivyaDiya12@users.noreply.github.com>
2020-09-02 11:31:06 +0530

    First Commit

--- Assignment4WebApp/Assignment4WebApp/Program.cs
+++ Assignment4WebApp/Assignment4WebApp/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace SampleApp
+namespace Assignment4WebApp
 {
     public class Program
     {
--- Assignment4WebApp/Assignment4WebApp/Startup.cs
+++ Assignment4WebApp/Assignment4WebApp/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace SampleApp
+namespace Assignment4WebApp
 {
     public class Startup
     {
